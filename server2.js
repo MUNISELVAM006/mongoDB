39742a77b11b7645a7b04c978a7edbe46794b7f2
MUNISELVAM006 <muniselvam006@gmail.com>
2024-11-06 11:34:09 +0530

    first commit

---
+++ server2.js
@@ -0,0 +1,31 @@
+// import express from "express"
+// import mongoose  from "mongoose"
+// import cors from "cors"
+// import dotenv from "dotenv"
+// dotenv.config()
+
+
+// // const app = express()
+// express().use(express.json())
+// express().use(cors())
+
+// express().use(express.urlencoded({extended : true }))
+
+// const uri = process.env.MONGODB_URI
+
+// mongoose
+// .connect(uri)
+// .then(()=>{
+//     console.log("mongoDb connected")
+// })
+// .catch ((err)=>{
+//     console.log("mondoDB not connected", err)
+// })
+
+// const PORT = process.env.PORT 
+
+// express().listen(PORT,()=>{
+//     console.log(`Server running on http://localhost:${PORT}`)
+// })
+
+
